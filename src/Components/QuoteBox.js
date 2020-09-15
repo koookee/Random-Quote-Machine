f8c9e7f590c7b61a347181fb6741ce17754db5bc
koookee <55768904+koookee@users.noreply.github.com>
2020-09-15 04:24:52 -0400

    Trying to figure out flexbox

--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -4,15 +4,20 @@ import styles from '../CSS/quoteBox.module.css';
 function QuoteBox(){
   let center = "center";
   let flexEnd = "flex-end";
-  function boxDisplay(justify){ return {backgroundColor:"#FF66CC",display:"flex",justifyContent:justify, alignItems:"center",border:"solid black",flexWrap:"wrap"}}
-  let textDisplay = {flexGrow:0, border:"solid black",textAlign:center,alignSelf:"center",height:"7em"};
-  let authorDisplay = {border:"solid black"};
-  let genDisplay = {width:"10em",height:"3em",border:"solid black",alignSelf:"flex-end"};
+
+  function boxDisplay(justify){ return {backgroundColor:"#FF66CC"
+  ,margin:"6em",marginTop:"15em",marginBottom:"15em",display:"flex",height:"10em",
+  justifyContent:"space-around", alignItems:"center",border:"solid black",flexWrap:"wrap"}}
+
+  let textDisplay = {flexGrow:4, border:"solid black",textAlign:"center",alignSelf:"stretch",flexShrink:1};
+  let authorDisplay = {flexGrow:4,border:"solid black",alignSelf:"flexEnd",flexShrink:1};
+  let genDisplay = {flexGrow:0,width:"8em",border:"solid black",flexShrink:1};
+
   return(
     <div>
-      <div style={{display:"flex",justifyContent:"center", alignItems:"center",backgroundColor:"blue" }}>
-        <div id="quote-box" style={boxDisplay(center,center)}>
-          <p id="text" style={textDisplay}>Hello world</p>
+      <div>
+        <div id="quote-box" style={boxDisplay("spaceAround")}>
+          <p id="text" style={textDisplay}>Today is bla bla bla blaaa bla, swvfiwf, WNKVBYIWABV, ABWIHYVBYIWVBguvg  sgabcgadbcadw</p>
           <p id="author" style={authorDisplay}> - author </p>
           <input id="new-quote" type="button" style={genDisplay} value="Generate" />
         </div>

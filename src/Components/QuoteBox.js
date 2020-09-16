7899e34183b38dc7e3607d795a6fd4388ab2e7c8
koookee <55768904+koookee@users.noreply.github.com>
2020-09-16 02:56:25 -0400

    Removed most margins that broke the layout. Added media query. CSS of the box is almost done

--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -6,20 +6,19 @@ function QuoteBox(){
   let flexEnd = "flex-end";
 
   function boxDisplay(justify){ return {backgroundColor:"#FF66CC"
-  ,margin:"6em",marginTop:"15em",marginBottom:"15em",display:"flex",height:"10em",
+  ,margin:"6em",marginTop:"15em",marginBottom:"15em",display:"flex",height:"12em",
   justifyContent:"space-around", alignItems:"center",border:"solid black",flexWrap:"wrap"}}
 
-  let textDisplay = {flexGrow:4, border:"solid black",textAlign:"center",alignSelf:"stretch",flexShrink:1};
-  let authorDisplay = {flexGrow:4,border:"solid black",alignSelf:"flexEnd",flexShrink:1};
-  let genDisplay = {flexGrow:0,width:"8em",border:"solid black",flexShrink:1};
-
+  let textDisplay = {border:"solid black",textAlign:"center",alignSelf:"stretch",flex:"2 10 auto"};
+  let authorDisplay = {border:"solid black",alignSelf:"flexEnd",flex:"4 0 auto"};
+  let genDisplay = {height:"2em",border:"solid black",flex:"4 1 0"};
   return(
     <div>
       <div>
         <div id="quote-box" style={boxDisplay("spaceAround")}>
-          <p id="text" style={textDisplay}>Today is bla bla bla blaaa bla, swvfiwf, WNKVBYIWABV, ABWIHYVBYIWVBguvg  sgabcgadbcadw</p>
-          <p id="author" style={authorDisplay}> - author </p>
-          <input id="new-quote" type="button" style={genDisplay} value="Generate" />
+          <p id="text" style={textDisplay}>Text zz text qqq qqq qqqqq qqq, qqqqqqq, qqqqqqqqq, QQQQQQQQQQQQQQQ  qqqqqqqqqq</p>
+          <p id="author" style={authorDisplay}> - Test Testing  </p>
+          <input id="new-quote" className={styles.genBox} type="button" style={genDisplay} value="Generate" />
         </div>
       </div>
     </div>

76ec43ad2438b72696a2fa63d81c963f406970b6
koookee <55768904+koookee@users.noreply.github.com>
2020-09-17 02:53:34 -0400

    Added a twitter link and tried importing Bootstrap

--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import styles from '../CSS/quoteBox.module.css';
+import Button from 'react-bootstrap/Button';
 
-function QuoteBox(){
-  let center = "center";
-  let flexEnd = "flex-end";
-
-  function boxDisplay(justify){ return {backgroundColor:"#FF66CC"
+class QuoteBox extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      quote:"Testing"
+    }
+  }
+render(){
+  function boxDisplay(justifyType){ return {backgroundColor:"#FF66CC"
   ,margin:"6em",marginTop:"15em",marginBottom:"15em",display:"flex",height:"12em",
-  justifyContent:"space-around", alignItems:"center",border:"solid black",flexWrap:"wrap"}}
+  justifyContent:justifyType, alignItems:"center",border:"solid black",flexWrap:"wrap"}}
 
   let textDisplay = {border:"solid black",textAlign:"center",alignSelf:"stretch",flex:"2 10 auto"};
   let authorDisplay = {border:"solid black",alignSelf:"flexEnd",flex:"4 0 auto"};
   let genDisplay = {height:"2em",border:"solid black",flex:"4 1 0"};
+
+  let twitterLink = "https://twitter.com/intent/tweet?text=Quote of the day: " + this.state.quote;
+
   return(
     <div>
       <div>
@@ -19,10 +27,11 @@ function QuoteBox(){
           <p id="text" style={textDisplay}>Text zz text qqq qqq qqqqq qqq, qqqqqqq, qqqqqqqqq, QQQQQQQQQQQQQQQ  qqqqqqqqqq</p>
           <p id="author" style={authorDisplay}> - Test Testing  </p>
           <input id="new-quote" className={styles.genBox} type="button" style={genDisplay} value="Generate" />
+          <a id="tweet-quote" href={twitterLink} target="_blank">tweet</a>
         </div>
       </div>
     </div>
-  )
+  )}
 }
 
 export default QuoteBox;

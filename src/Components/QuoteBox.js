26b27a83c518511820a9ec9c71761e3d0250fd7e
koookee <55768904+koookee@users.noreply.github.com>
2020-09-18 08:25:52 -0400

    Added ability to fetch data from an API for the quotes and authors. If the returned author data is null, I replaced it with 'Unknown'. All that's left are a few style changes to make the site look better.

--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -6,28 +6,46 @@ class QuoteBox extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      quote:"Testing"
+      quote:"Welcome to the random quote generator! Click on 'Generate' to display a new random quote",
+      author:"Hussein El Mokdad",
+      isLoading:false
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    this.setState({isLoading:true})
+    fetch("https://type.fit/api/quotes")
+    .then(response => response.json())
+    .then(data => {
+      let randomNum = Math.floor(Math.random() * 1643);
+      //1643 is the arr length
+      if(data[randomNum].author == null){
+        this.setState({quote:data[randomNum].text,author:"Unknown"})
+      }
+      else{
+        this.setState({quote:data[randomNum].text,author:data[randomNum].author,isLoading:false})}
+    })
   }
 render(){
   function boxDisplay(justifyType){ return {backgroundColor:"#FF66CC"
   ,margin:"6em",marginTop:"15em",marginBottom:"15em",display:"flex",height:"12em",
   justifyContent:justifyType, alignItems:"center",border:"solid black",flexWrap:"wrap"}}
 
-  let textDisplay = {border:"solid black",textAlign:"center",alignSelf:"stretch",flex:"2 10 auto"};
-  let authorDisplay = {border:"solid black",alignSelf:"flexEnd",flex:"4 0 auto"};
-  let genDisplay = {height:"2em",border:"solid black",flex:"4 1 0"};
+  let textDisplay = {textAlign:"center",alignSelf:"stretch",flex:"2 10 auto",flexWrap:"wrap"};
+  let authorDisplay = {flex:"4 0 auto"};
+  let genDisplay = {height:"2em",flex:"4 1 0"};
+  let twitterDisplay = {alignSelf:"flex-end",flex:"1 1 1"}
 
-  let twitterLink = "https://twitter.com/intent/tweet?text=Quote of the day: " + this.state.quote;
+  let twitterLink = "https://twitter.com/intent/tweet?text=Quote of the day: " + this.state.quote + " - " + this.state.author;
 
   return(
     <div>
       <div>
         <div id="quote-box" style={boxDisplay("spaceAround")}>
-          <p id="text" style={textDisplay}>Text zz text qqq qqq qqqqq qqq, qqqqqqq, qqqqqqqqq, QQQQQQQQQQQQQQQ  qqqqqqqqqq</p>
-          <p id="author" style={authorDisplay}> - Test Testing  </p>
-          <input id="new-quote" className={styles.genBox} type="button" style={genDisplay} value="Generate" />
-          <a id="tweet-quote" href={twitterLink} target="_blank">tweet</a>
+          <p id="text" style={textDisplay}>{this.state.isLoading?"Fetching...":this.state.quote}</p>
+          <p id="author" style={authorDisplay}> -{this.state.isLoading?"Thank you for waiting!":this.state.author}</p>
+          {this.state.isLoading?null:<input id="new-quote" className={styles.genBox} type="button" style={genDisplay} value="Generate" onClick={this.handleClick} />}
+          {this.state.isLoading?null:<a id="tweet-quote" href={twitterLink} target="_blank" style={twitterDisplay}>tweet</a>}
         </div>
       </div>
     </div>

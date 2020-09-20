c30bd1d2a497bfbc9b2ee3ee1e42076b681fa8a1
koookee <55768904+koookee@users.noreply.github.com>
2020-09-20 03:03:00 -0400

    Added bootstrap and changed the font of the quote text

--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from '../CSS/quoteBox.module.css';
+//import Button from 'bootstrap/dist/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
+
 
 class QuoteBox extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      quote:"Welcome to the random quote generator! Click on 'Generate' to display a new random quote",
+      quote:"Welcome to the random quote generator! Click on \"Generate\" to display a new random quote",
       author:"Hussein El Mokdad",
       isLoading:false,
       showTwitterLink:false
@@ -32,9 +34,9 @@ render(){
   function boxDisplay(justifyType){ return {backgroundColor:"#FE91CA",display:"flex",height:"12em",
   margin:"5em 5em",justifyContent:justifyType, alignItems:"center",border:"solid black",flexWrap:"wrap"}}
 
-  let textDisplay = {textAlign:"center",alignSelf:"stretch",flex:"2 10 auto",flexWrap:"wrap"};
-  let authorDisplay = {flex:"4 0 auto"};
-  let genDisplay = {height:"2em",flex:"4 1 0"};
+  let textDisplay = {textAlign:"center",alignSelf:"stretch",flex:"2 10 auto",flexWrap:"wrap",fontFamily:"Arial"};
+  let authorDisplay = {flex:"4 0 auto",fontStyle:"oblique"};
+  let genDisplay = {flex:"4 1 0"};
   let twitterDisplay = {alignSelf:"flex-end",flex:"1 1 1"}
 
   let twitterLink = "https://twitter.com/intent/tweet?text=Quote of the day: " + this.state.quote + " - " + this.state.author;
@@ -45,8 +47,8 @@ render(){
         <div id="quote-box" style={boxDisplay("spaceAround")}>
           <p id="text" style={textDisplay}>{this.state.isLoading?"Fetching...":this.state.quote}</p>
           <p id="author" style={authorDisplay}> -{this.state.isLoading?"Thank you for waiting!":this.state.author}</p>
-          {this.state.isLoading?null:<input id="new-quote" className={styles.genBox} type="button" style={genDisplay} value="Generate" onClick={this.handleClick} />}
-          {this.state.isLoading?null:(this.state.showTwitterLink?<a id="tweet-quote" href={twitterLink} target="_blank" style={twitterDisplay}>tweet</a>:null)}
+          {this.state.isLoading?null:<Button id="new-quote" variant="dark" className={styles.genBox} style={genDisplay} onClick={this.handleClick}>Generate</Button>}
+          {this.state.isLoading?null:(this.state.showTwitterLink?<Button variant="primary" size="sm" id="tweet-quote" href={twitterLink} target="_blank" style={twitterDisplay}>Tweet</Button>:null)}
         </div>
       </div>
     </div>
